ca18bcae2a29559b986d6d62b364fde7b39c971d
PeterVanhoolst <peter.vanhoolst@gmail.com>
2023-09-29 10:17:24 +0200

    Update taal.mjs

--- taal.mjs
+++ taal.mjs
@@ -2,9 +2,6 @@ import * as readline from 'node:readline/promises';
 import{stdin as input, stdout as output} from 'node:process';
 const userInput = readline.createInterface({input, output});
 
-
-
-
 let geldigeTaal = await userInput.question("Maak uw keuze tussen NL, FR en EN.");
 
 if(geldigeTaal == "NL"){
